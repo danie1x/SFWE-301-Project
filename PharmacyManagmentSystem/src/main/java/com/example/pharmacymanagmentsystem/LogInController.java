8602b8defa515607996c2a6c231de956e6535b0b
danie1x <131409290+danie1x@users.noreply.github.com>
2024-11-23 18:04:29 -0700

    Simple log in page created!

---
+++ PharmacyManagmentSystem/src/main/java/com/example/pharmacymanagmentsystem/LogInController.java
@@ -0,0 +1,35 @@
+package com.example.pharmacymanagmentsystem;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LogInController {
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    protected void onLoginButtonClick() throws IOException {
+        // Load the hello-view.fxml
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        // Get the current stage and set the new scene
+        Stage stage = (Stage) loginButton.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Hello!");
+        stage.show();
+    }
+}
